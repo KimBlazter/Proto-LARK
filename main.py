3cefd2dad54e4da941e0b673057309a7f534f6e5
Simon Petrequin <simon.petrequin@icloud.com>
2025-10-15 20:47:28 +0200

    feat: parse un-ordered params

--- main.py
+++ main.py
@@ -24,8 +24,40 @@ class GCodeGenerator(Transformer):
     
     def start(self, *commands):
         return "\n".join(str(c) for (c) in commands)
+    
+    def gcode_block(self, *commands):
+        return "\n".join(str(c) for (c) in commands)
 
+    def gcommand(self, *params):
+        if len(params) != 4:
+            raise SyntaxError("Must have exactly 4 parameters")
+        var_type = [p[0] for p in params]
+        var_values = [p[1] for p in params]
+        expected = {"X", "Y", "Z", "S"}
+        seen = set(var_type)
 
+        if seen != expected:
+            missing = expected - seen
+            extra = [v for v in var_type if var_type.count(v) > 1]
+            msg = []
+            if missing:
+                msg.append(f"Missing: {', '.join(missing)}")
+            if extra:
+                msg.append(f"Duplicates: {', '.join(set(extra))}")
+            raise ValueError("Invalid gcommand: " + "; ".join(msg))
+        return f"G0 {" ".join([p[0] + str(p[1]) for p in params])}"
+        
+    def gparam_x(self, value):
+        return ("X", value)
+    
+    def gparam_y(self, value):
+        return ("Y", value)
+    
+    def gparam_z(self, value):
+        return ("Z", value)
+    
+    def gparam_speed(self, value):
+        return ("S", value)
 
 def load_grammar():
     """Load grammar for a .lark file"""
@@ -46,6 +78,19 @@ def transpile(pgcode_source):
         tree = parser.parse(pgcode_source)
         generator = GCodeGenerator()
         gcode = generator.transform(tree)
+        print(f'''
+----------------
+
+TREE:
+{tree.pretty()}
+
+----------------
+
+TRANSFORMED:
+{gcode}
+
+----------------
+        ''')
         return gcode
     except Exception as e:
         print(f"Erreur de transpilation: {e}")
@@ -53,13 +98,9 @@ def transpile(pgcode_source):
     
 def main():
     code = """
-    print on
-    move x=1 y=2 z=3
-    print off
-    move z=10
-    pause
+    G0 Y5.458 Z2.000 S8.140 X1.021
     """
-    print(transpile(code))
+    transpile(code)
     
     
 
